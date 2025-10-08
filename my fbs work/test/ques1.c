15322463b46e1f359f5e9d4a54bab8efe01df336
Aryannikam03 <141949746+Aryannikam03@users.noreply.github.com>
2025-10-08 18:02:43 +0530

    Add files via upload

---
+++ my fbs work/test/ques1.c
@@ -0,0 +1,17 @@
+//WAP print even and odd numbers
+
+void main()
+{
+	int i;
+	for(int i=10;i<=25;i++)
+	{
+		if(i%2==0)
+		{
+			printf("even number:%d\n",i);
+		}
+		else
+		{
+			printf("odd number:%d\n",i);
+		}
+	}
+}
---
+++ my fbs work/test/ques2.c
@@ -0,0 +1,13 @@
+//WAP to add alternate numbers from given range
+
+void main()
+{
+	int i;
+	int sum=0;
+	for(int i=1;i<=5;i++)
+	{
+		i=i+2;
+		sum=sum+i;
+	}
+	printf("%d",sum);
+}
